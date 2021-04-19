a7372791923944e3f5a572dcc579750f89c1e03e
Anubhav Chhabra <chhabra.anubhav1997@gmail.com>
2021-04-18 20:20:57 -0400

    [ReactExpenseTracker] - Structure components

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+
+function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.title)
+
+    const clickHandler = () => {
+        setTitle("Updated Value")
+        console.log(title);
+    }
+
+    return (
+    <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+            <h2>{title}</h2>
+            <div className="expense-item__price">${props.price}</div>
+        </div>
+        <button onClick={clickHandler}>Button</button>
+    </Card>
+    );
+} 
+
+export default ExpenseItem;
---
+++ src/components/Expenses/ExpenseList.js
@@ -0,0 +1,20 @@
+import './ExpenseList.css';
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card'
+
+function ExpenseList(props) {
+    const itemList = props.itemList
+    console.log(itemList)
+
+    return (
+        <Card className="expenses">
+            <ExpenseItem title={itemList[0].title} date={itemList[0].date} price={itemList[0].price}></ExpenseItem>
+            <ExpenseItem title={itemList[1].title} date={itemList[1].date} price={itemList[1].price}></ExpenseItem>
+            <ExpenseItem title={itemList[2].title} date={itemList[2].date} price={itemList[2].price}></ExpenseItem>
+      </Card>
+    )
+
+}
+
+
+export default ExpenseList;
